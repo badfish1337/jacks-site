4ea888b6043ba0e6694eb1e5e67afb83ec13b991
Chris Dewey <85541441+badfish1337@users.noreply.github.com>
2022-04-30 20:41:47 -0700

    add newsletter-signup

---
+++ newsletter-signup/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    console.log(firstName + " " + lastName + " " + email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us18.api.mailchimp.com/3.0/lists/d0541d5165";
+
+    const options = {
+        method: "POST",
+        auth: "codeguide:52cac90e28cb53ad1c5214233d6a6a5e-us18"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+// Work on both local port 3000 and dynamically when hosted
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000.");
+});
+
